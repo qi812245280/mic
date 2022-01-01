dfb08f161ae1ba744ee3e37400cf68e88791199b
qi812245280 <qi812245280@163.com>
2022-01-01 20:35:11 +0800

    .

--- golang_code/q010/main.go
+++ golang_code/q010/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
 GO里面MAP如何实现key不存在 get操作等待 直到key存在或者超时，保证并发安全，且需要实现以下接口：
 
@@ -9,3 +15,76 @@ type sp interface {
     Rd(key string, timeout time.Duration) interface{}  //读取一个key，如果key不存在阻塞，等待key存在或者超时
 }
 */
+type entry struct {
+	Value   interface{}
+	Ch      chan struct{}
+	IsExtra bool
+}
+
+type Map struct {
+	data map[string]*entry
+	lock *sync.RWMutex
+}
+
+func (m *Map) Out(key string, val interface{}) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	if v, ok := m.data[key]; !ok {
+		newE := &entry{
+			Value:   val,
+			Ch:      make(chan struct{}),
+			IsExtra: true,
+		}
+		m.data[key] = newE
+		newE.Ch <- struct{}{}
+	} else {
+		v.IsExtra = true
+		v.Value = val
+		v.Ch <- struct{}{}
+	}
+}
+
+func (m *Map) Rd(key string, timeout time.Duration) interface{} {
+	m.lock.Lock()
+	if v, ok := m.data[key]; ok {
+		if v.IsExtra {
+			m.lock.Unlock()
+			return v.Value
+		} else {
+			m.lock.Unlock()
+			fmt.Println("阻塞中...")
+			select {
+			case <-v.Ch:
+				return v.Value
+			case <-time.After(timeout):
+				fmt.Println("阻塞超时")
+				return nil
+			}
+		}
+	}
+	newE := &entry{
+		Value:   nil,
+		Ch:      make(chan struct{}),
+		IsExtra: false,
+	}
+	m.data[key] = newE
+	m.lock.Unlock()
+	fmt.Println("阻塞中...")
+	select {
+	case <-newE.Ch:
+		return newE.Value
+	case <-time.After(timeout):
+		fmt.Println("阻塞超时")
+		return nil
+	}
+}
+
+func main() {
+	m := Map{data: make(map[string]*entry), lock: &sync.RWMutex{}}
+	go func() {
+		fmt.Println(m.Rd("qi", 10*time.Second))
+	}()
+	time.Sleep(1 * time.Second)
+	m.Out("qi", "哎呀呀")
+	time.Sleep(10 * time.Second)
+}
